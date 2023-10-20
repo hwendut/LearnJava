8cd393dc1fcc3eda4047d7abf1637e6e47892919
hwen_Laptop <wenhaodut@163.com>
2023-10-20 23:05:44 +0800

    Update P39

--- 黑马教程/代码/basic-code/day03-code/src/com/heima/operator/OperatorDemo1.java
+++ 黑马教程/代码/basic-code/day03-code/src/com/heima/operator/OperatorDemo1.java
@@ -1,6 +1,6 @@
 package com.heima.operator;
 
-public class OperatorDemo {
+public class OperatorDemo1 {
     public static void main(String[] args) {
         // 浮点数参与计算，结果有可能不精确
         System.out.println(1.1 + 1.01); // 2.1100000000000003
