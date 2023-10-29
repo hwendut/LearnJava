dda777565788c1e2844fb0565a464bd3db1c03c7
hwen_Laptop <wenhaodut@163.com>
2023-10-29 22:51:27 +0800

    Update P108

--- 黑马教程/代码/basic-code/day10-code/src/com/heima/demo/StringDemo12.java
+++ 黑马教程/代码/basic-code/day10-code/src/com/heima/demo/StringDemo12.java
@@ -5,5 +5,13 @@ public class StringDemo12 {
         String talk = "你玩的真彩，TMD";
         String result = talk.replace("TMD", "***");
         System.out.println(result);
+        byte[] arr = {1, 2, 3};
+        String str = new String(arr);
+        System.out.println(str);
+
+        StringBuilder sb = new StringBuilder();
+        String str1 = "";
+        sb.append(str1);
+        System.out.println(sb);
     }
 }
---
+++ 黑马教程/代码/basic-code/day10-code/src/com/heima/test/Test1.java
@@ -0,0 +1,18 @@
+package com.heima.test;
+
+import java.util.Scanner;
+
+public class Test1 {
+    public static void main(String[] args) {
+        /* 键盘录入一个字符串
+        *  要求1：长度小于等于9
+        *  要求2：只能是数字
+        *  将内容变成罗马数字
+        *  罗马数字里面没有0
+        *  如果键盘录入的数字包含0，可以变成""（长度为0的字符串）*/
+        System.out.println("请输入一个字符串");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+    }
+}
