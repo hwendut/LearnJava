6fa6bbd824519115503c2a2c45108dd844e336cd
hwen <wenhaodut@163.com>
2023-11-09 16:54:32 +0800

    Update P141

---
+++ 黑马教程/代码/basic-code/Day14-code/src/com/heima/Outer.java
@@ -0,0 +1,16 @@
+package com.heima;
+
+public class Outer {
+    private int a = 10;
+
+    class Inner {
+        private int a = 20;
+
+        public void show() {
+            int a = 30;
+            System.out.println(Outer.this.a);
+            System.out.println(this.a);
+            System.out.println(a);
+        }
+    }
+}
--- 黑马教程/代码/basic-code/Day14-code/src/com/heima/Test.java
+++ 黑马教程/代码/basic-code/Day14-code/src/com/heima/Test.java
@@ -1,12 +1,20 @@
 package com.heima;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
-        Fu a = new Zi();
+        /*Fu a = new Zi();
         a.print();
         // a.eat();
         Zi b = new Zi("zhangsan");
         b.printFu();
-        b.eat();
+        b.eat();*/
+
+        Outer.Inner oi = new Outer().new Inner();
+        oi.show();
+
+        Scanner sc = new Scanner(System.in);
+        sc.next();
     }
 }
